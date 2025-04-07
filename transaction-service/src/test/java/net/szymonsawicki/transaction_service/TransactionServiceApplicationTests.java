7fd45a377c2f520278eeb9657103552300ec954c
szymon-sawicki <szymonsawick@gmail.com>
2025-04-07 22:06:48 +0200

    init commit

---
+++ transaction-service/src/test/java/net/szymonsawicki/transaction_service/TransactionServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package net.szymonsawicki.transaction_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TransactionServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
